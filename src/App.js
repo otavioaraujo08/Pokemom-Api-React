f4a8ece9bedee76996707c4620b881e8f20c42ed
Otávio Araujo <51797012+otavioaraujo08@users.noreply.github.com>
2022-06-10 10:04:47 -0300

    13. Alterações na home e remoção do componente de sidebar

--- src/App.js
+++ src/App.js
@@ -1,20 +1,14 @@
-import ListPokemons from "./components/ListPokemons";
+// import ListPokemons from "./components/ListPokemons";
 import Home from "./components/template/home";
-import Drawer from './components/template/drawer/index'
+import ListPokemons from "./components/ListPokemons";
 import './App.css'
 
 function App() {
   return (
     <>
-      <header>
-        <Home />
-      </header>
-
-      <div className="home">
-        <Drawer />
-        <ListPokemons />
-      </div>
-    
+      <Home />
+      
+      <ListPokemons />  
     </>
   );
 }
