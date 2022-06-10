154734c96f49b6c559c566f70d981d9989073bde
Otávio Araujo <51797012+otavioaraujo08@users.noreply.github.com>
2022-06-10 10:05:25 -0300

    14. Criação e definição do componente "Home"

--- src/components/template/home/index.js
+++ src/components/template/home/index.js
@@ -1,13 +1,94 @@
 
 import './index.css'
+import Logo from './poke.png'
+import Charmander from './charmander.png'
+import { Box, Button, Paper, Typography } from '@mui/material';
+import LocalFireDepartmentIcon from '@mui/icons-material/LocalFireDepartment';
+import FormatColorResetIcon from '@mui/icons-material/FormatColorReset';
+import PetsIcon from '@mui/icons-material/Pets';
 
 function Home() {
     return(
-        <div className='home'>
-            <header>
-                <h1>Olá Mundo</h1>
-            </header>
-        </div>
+        <>
+            <div className='home'>
+                <header className='header'>
+                    <img 
+                    src={Logo} 
+                    alt="Pokemon-Logo"
+                    width="14%"
+                    />
+                </header>
+
+                <div className='body'>
+                    <div class="div1">
+                        <h2>Charmander</h2>
+                        
+                        <Box
+                            sx={{
+                                display: 'flex',
+                                alignItems: 'center',
+                                flexDirection: 'column',
+                            }}
+                        >
+                            <Button 
+                                variant="contained" 
+                                color="error" 
+                                className='buttons'
+                                startIcon={<PetsIcon />}
+                            >
+                                Categoria: Lagarto
+                            </Button>
+                            <Button 
+                                variant="contained" 
+                                color="error" 
+                                className='buttons'
+                                startIcon={<LocalFireDepartmentIcon />}
+                            >
+                                
+                                Evolui para: Charmeleon
+                            </Button>
+                            <Button 
+                                variant="contained" 
+                                color="error" 
+                                className='buttons'
+                                startIcon={<FormatColorResetIcon />}
+                            >
+                                Fraqueza: Água
+                            </Button>
+                            
+                            <Paper
+                                sx = {{
+                                    bgcolor: 'transparent',
+                                    borderRadius: 5,
+                                }}
+                                className="paper"
+                            >
+                                <Typography 
+                                    variant="h6"
+                                    gutterBottom
+                                    component="div"
+                                    sx = {{
+                                        m: 7,
+                                    }}
+                                    className='subTitulo'
+                                >
+                                    Charmander é um Pokémon do Tipo Fogo e um dos Pokémon capturáveis do jogo Pokémon Go. Ele evolui para Charmeleon no nível 16 e para Charizard no nível 36.
+                                </Typography>
+                            </Paper>
+                        </Box>
+                        
+                    </div>
+                    
+                    <div class="div2">
+                        <img 
+                        src={Charmander} 
+                        alt="Pokemon-Logo"
+                        width="60%"
+                        />
+                    </div> 
+                </div>
+            </div>
+        </>
     )
 }
 
