e4e3f45742c8bab174f5369c82155019df04349e
Otávio Araujo <51797012+otavioaraujo08@users.noreply.github.com>
2022-06-10 14:25:55 -0300

    17. Pokemon Details

--- src/components/PokemomDetails/index.js
+++ src/components/PokemomDetails/index.js
@@ -1,8 +1,19 @@
+import './index.css'
+import { Box, LinearProgress, Typography } from '@mui/material';
 import { useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom' 
 import api_individual from '../../services/apiIndividual';
 
 function PokemonsDetails() {
+    const cores = [
+        '#91ff35',
+        '#ff6333',
+        '#637bfe',
+        '#b2102f',
+        '#4615b2',
+        '#ffea00',
+    ]
+
     const { id } = useParams()
 
     // Nossa lista que sera preenchida pela requisição
@@ -14,7 +25,7 @@ function PokemonsDetails() {
         api_individual.get(id).then(({data}) => {
             setList(data)
 
-            console.log(data)
+            console.log(data.stats)
         })
     // eslint-disable-next-line react-hooks/exhaustive-deps
     }, []);
@@ -22,18 +33,77 @@ function PokemonsDetails() {
     return(
         <>
             {id && (
-                <>
-                    <ul>
-                        <li>
-                            {list.name}
-                        </li>
-                        <img src={list?.sprites?.front_default} alt={list.name}/>
-                        <p>
-                            {list?.types?.map((item) => <li>{item.type.name}</li>)}
-                        </p>
-                    </ul>
-                </>
-            )}
+                <Box className='box'>
+                    <Box className="informacoesUm">
+                        <Box className="nome">
+                            <Typography variant="h4">
+                                {list.name}
+                            </Typography>
+                        </Box>
+                        <Box className="imagen">
+                            <img 
+                                src={list?.sprites?.front_default} 
+                                alt={list.name} 
+                                width="70%"
+                            />
+                        </Box>
+                        <Box className="tipo">
+                            <Typography 
+                                variant="h5"
+                                sx={{ m: 1 }}
+                            >
+                                #{list.id} 
+                            </Typography>
+
+                            <Typography 
+                                variant="h5"
+                                sx={{ m: 1 }}
+                            >
+                                Pokemon type:  {list?.types?.map((item) => item.type.name.concat(' '))}
+                            </Typography>
+                        </Box>
+                    </Box>
+                    <Box className='informacoesDois'>
+                        <Box className='infos'>
+                            <Typography 
+                                variant="h4"
+                                sx={{
+                                    textAlign: 'center',
+                                }}
+                            >
+                                Infos
+                            </Typography>
+                            <Box className='estatisticas'>
+                                {list?.stats?.map((item, index) => { 
+                                    return(
+                                        <Box key={item.stat.name} className='status'>
+                                            <Typography 
+                                                variant="h6"
+                                                sx={{ ml: 1}}
+                                            >
+                                                {item.stat.name}: {item.base_stat}
+                                            </Typography>
+
+                                            <LinearProgress 
+                                                variant="determinate"
+                                                value={item.base_stat} 
+                                                sx={{
+                                                    width: 400,
+                                                    height: 10, 
+                                                    mr: 2,
+                                                    background: cores[index]
+                                                }} 
+                                            
+                                            />
+                                        </Box>
+                                    )
+                                })}
+                            </Box>
+                        </Box>
+                    </Box>
+                </Box>
+            )}            
+        
         </>
     )
 }
